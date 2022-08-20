756e5dec1026c7d4cc8f5158f728eb1b8c05bc64
Irina <avis.tenera@gmail.com>
2022-08-20 21:32:00 +0300

    Добавили задачу 29

---
+++ Task29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Вводим массив через консоль
+
+Console.WriteLine("Введите восемь элементов массива через пробел");
+string N = Console.ReadLine();
+int i = 0;
+
+string[] array = N.Split(" ");
+if (array.Length != 8)
+{
+    Console.WriteLine("Данные введены неверно!");
+}
+else
+{
+
+    for (i = 0; i < 8; i++)
+    {
+        if (i != 7)
+        {
+            Console.Write(array[i] + ", ");
+        }
+
+        else
+        {
+            Console.Write(array[i]);
+        }
+
+    }
+}
