0844accdfbaccbd33208d729ffd82907e819cc39
Irina <avis.tenera@gmail.com>
2022-08-20 21:32:41 +0300

    Добавили дополнительную задачу из прошлой домашки

---
+++ Extra_task/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа для разворота массива
+
+int[] array = { 2,3,5,6 };
+int N = array.Length;
+for (int i=0; i<N/2; i++)
+{
+int temp = array[i];
+array[i]=array[N - i - 1];
+array[N - i - 1]=temp;
+}
+for (int i = 0; i<N; i++)
+{
+Console.WriteLine(array[i]);
+}
